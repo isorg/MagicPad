6e77bff3d6cd8c50c6d42b44163708343f0490ce
Adrien Bailly <adrien.bailly@gmail.com>
2012-05-07 15:51:58 +0200

    Removing Qwt3D

--- src/applets/surfaceApplet.h
+++ src/applets/surfaceApplet.h
@@ -3,20 +3,19 @@
 
 #include "applet.h"
 
-#include "filters/calibrationFilter.h"
+#include <QPainter>
 
-#include <qwt3d_surfaceplot.h>
-#include <qwt3d_function.h>
+#include "filters/calibrationFilter.h"
 
 #define SURFACE_NAME "surface"
 #define SURFACE_TITLE "Surface"
 
-#define SURFACE_UPSCALING_FACTOR 3
-
-#define SURFACE_PLOTFACTOR 20
-
+/**
+ *
+ */
 class SurfaceApplet : public Applet
 {
+
     Q_OBJECT
 
 public:
@@ -26,6 +25,7 @@ public:
     SurfaceApplet(QWidget *parent = 0);
 
 public slots:
+
     void setFrame( cv::Mat& frame );
 
     void paintEvent(QPaintEvent *) {
@@ -39,19 +39,8 @@ private:
     void setupSurface();
 
 private:
-    // algo
-    CalibrationFilter *mCalibrationFilter;
-    cv::Mat mFrame;
-    cv::Mat mFrameFiltered;
-    cv::Mat mFrameDouble;
-    cv::Mat mHighResFrame;
 
-    // view
-    Qwt3D::SurfacePlot *mSurface;
-    Qwt3D::ColorVector mColorVector;
-    double **mTable;
-    int m_hrWidth;
-    int m_hrHeight;
+    CalibrationFilter *mCalibrationFilter;
 };
 
 #endif // SURFACEAPPLET_H
